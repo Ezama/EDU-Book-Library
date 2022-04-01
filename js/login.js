4c320b59104b37e3fec83ec8e28cd69bce1e6985
Ezama <ezama.eazye@gmail.com>
2022-04-01 07:43:53 +0300

    added library functionality

--- js/login.js
+++ js/login.js
@@ -2,12 +2,14 @@ document.addEventListener("DOMContentLoaded", e => {
     const loginForm = document.querySelector("#login");
     const createAccountForm = document.querySelector("#createAccount");
 
+    //switch to signup page
     document.querySelector("#linkCreateAccount").addEventListener("click", e => {
         e.preventDefault();
         loginForm.classList.add("form-hidden");
         createAccountForm.classList.remove("form-hidden");
     });
 
+    //switch to login page
     document.querySelector("#linkLogin").addEventListener("click", e => {
         e.preventDefault();
         loginForm.classList.remove("form-hidden");
